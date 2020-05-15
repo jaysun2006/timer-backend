ececc890770fdba114b4b26ec42bda9dbda05f83
sanjay Kumar <sam@gmail.com>
2020-05-15 18:55:52 +0530

    Admin Change

--- booster/timer/views.py
+++ booster/timer/views.py
@@ -33,26 +33,45 @@ class TimerViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = super(TimerViewSet, self).get_queryset()
         queryset = queryset.filter(user=self.request.user)
-        if self.request.query_params.get('start') and self.request.query_params.get('end'):
-            start_date = get_day_start(parse_date(self.request.query_params.get('start')))
-            end_date = get_day_end(parse_date(self.request.query_params.get('end')))
-        else:
-            start_date = get_today_start()
-            end_date = get_today_end()
-        queryset = queryset.filter(start_datetime__gte=start_date, start_datetime__lte=end_date)
         return queryset
 
     def perform_create(self, serializer):
         instance = serializer.save(user=self.request.user)
 
 
+    @action(detail=False, methods=['get'])
+    def all_entry(self, request):
+        """Get all the complete Time Enry"""
+        if self.request.query_params.get('start') and self.request.query_params.get('end'):
+            start_date = get_day_start(parse_date(self.request.query_params.get('start')))
+            end_date = get_day_end(parse_date(self.request.query_params.get('end')))
+        else:
+            start_date = get_today_start()
+            end_date = get_today_end()
+        timers = self.queryset.filter(start_datetime__gte=start_date, start_datetime__lte=end_date)
+        return Response(TimerSerializer(timers, many=True).data)
+
+
     @action(detail=True, methods=['post'])
     def complete(self, request, pk=None):
-        """Perform For complete the Time Enry"""
-
+        """Perform For complete the Time Entry"""
         timer_obj = self.get_object()
         timer_obj.end_datetime = timezone.now()
         timer_obj.save()
         data = TimerSerializer(timer_obj).data
         logger.info("Time Entry" + str(timer_obj.pk) + "Completed Successfully")
         return Response({'status': 'Time Entry Ended Successfully'})
+
+
+    @action(detail=False, methods=['get'])
+    def active(self, request):
+        """Get all the incomplete  Time Enry"""
+        start_date = get_today_start()
+        end_date = get_today_end()
+        active_timers = Timer.objects.filter(user=self.request.user, end_datetime=None)
+        if  active_timers.exists():
+            data = TimerSerializer(active_timers[0]).data
+            return Response(data)
+        else:
+            logger.warning("Active entry doesn't exist'")
+            return Response()
