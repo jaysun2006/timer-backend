999a12512b37e927c65b52e40674d2d3b61844cb
sanjay Kumar <sam@gmail.com>
2020-05-15 15:22:18 +0530

    Adding New Filter for past entry

--- booster/timer/views.py
+++ booster/timer/views.py
@@ -1,11 +1,17 @@
+import datetime
 import logging
+
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.dateparse import parse_date
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
 from booster.timer.models import Timer
 from booster.timer.serializers import TimerSerializer
-from django.utils import timezone
+from booster.utils.timezone import (get_day_end, get_day_start, get_today_end,
+                                    get_today_start)
 
 logger = logging.getLogger(__name__)
 
@@ -19,16 +25,22 @@ class TimerViewSet(viewsets.ModelViewSet):
     For details of Time Entry
     For deleting Time Entry
     For updating Time Entry
-    
+
     """
     queryset = Timer.objects.all()
     serializer_class = TimerSerializer
 
     def get_queryset(self):
         queryset = super(TimerViewSet, self).get_queryset()
-        queryset = queryset.filter(user=self.request.user)
-        if self.request.query_params.get('start'):
-            queryset = queryset.filter(start_date=request.query_params.get('start'))
+        queryset = queryset.filter(user=self.request.user,)
+        if self.request.query_params.get('start') and self.request.query_params.get('end'):
+            print(self.request.query_params.get('start'), self.request.query_params.get('end'))
+            start_date = get_day_start(parse_date(self.request.query_params.get('start')))
+            end_date = get_day_end(parse_date(self.request.query_params.get('end')))
+        else:
+            start_date = get_today_start()
+            end_date = get_today_end()
+            queryset = queryset.filter(start_datetime__range=[start_date, end_date])
         return queryset
 
     def perform_create(self, serializer):
@@ -38,7 +50,7 @@ class TimerViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['post'])
     def complete(self, request, pk=None):
         """Perform For complete the Time Enry"""
-    
+
         timer_obj = self.get_object()
         timer_obj.end_datetime = timezone.now()
         timer_obj.save()
---
+++ booster/utils/timezone.py
@@ -0,0 +1,36 @@
+import datetime
+
+from django.utils import timezone
+
+
+def get_today_start():
+    """
+    :return: Start Date (YYYY-MM-DD HH:MM:SS): 2016-03-1 00:00:00
+    """
+    return timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def get_today_end():
+    """
+    :return: End Date (YYYY-MM-DD HH:MM:SS): 2016-03-15 23:59:59
+    """
+    tomorrow = get_today_start() + timedelta(days=1)
+    return tomorrow - timedelta(microseconds=1)
+
+
+
+def get_day_start(date):
+    """
+    :return: Start Date (YYYY-MM-DD HH:MM:SS): 2016-03-1 00:00:00
+    """
+
+    date_time = datetime.datetime.combine(date, datetime.time.min)
+    return date_time.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def get_day_end(date):
+    """
+    :return: End Date (YYYY-MM-DD HH:MM:SS): 2016-03-15 23:59:59
+    """
+    date_time = datetime.datetime.combine(date, datetime.time.min)
+    return date_time.replace(hour=23, minute=59, second=59, microsecond=0)
